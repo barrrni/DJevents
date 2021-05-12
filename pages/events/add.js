e9846fd4b560c8351f8518a0ee31694cb505eb2f
barrrni <barnabas.kiss@outlook.hu>
2021-05-12 14:34:34 +0200

    Login,register pages are up

--- pages/events/add.js
+++ pages/events/add.js
@@ -1,11 +1,12 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { useState } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Layout from '@/components/Layout'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Form.module.css'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+
 
 
 const addEventPage = () => {
